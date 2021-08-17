3db3dfbb5c47ab2dc86ffb2e900511089895069d
gokhangemici <gokhan_gemici@hotmail.com>
2021-08-17 18:11:06 +0300

    yaş grubu belirleme

--- app.php
+++ app.php
@@ -1,6 +1,6 @@
-<?php
+<?php // dosyamızın php diline ait olduğunu belirtmek için kullanılan açılış etiketi
 
-$age = 25;
+$age = 32; // php de değişkenler $ işareti ile tanımlanır
 
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
@@ -18,3 +18,28 @@ $age = 25;
  * 
  * İyi çalışmalar.
  */
+  
+
+
+
+    if($age >= 0 and $age <= 14) // and operatörü her iki koşul doğruysa çalışır. Diğer durumlarda çalışmaz.
+    {
+        echo "Çocuk"; // echo ekrana bir ifade yazdırmak için kullanılır. echo yerine print de kullanılabilir.
+    }
+    else if($age >= 15 and $age <= 24)
+    {
+        echo "Genç"; // tüm ifadeler noktali virgül ile sonlandırılmak zorundadır.
+    }
+    else if($age >= 25 and $age <= 64) // age değişkeni 32 değerine sahip olduğu için bu koşul içerisindeki kod bloğu çalışır ve ekrana yetişkin değerini yazar.
+    {
+        echo "Yetişkin";
+    }
+    else // yukardaki üç koşulun sağlanmadığı tüm durumlarda bu kod bloğu çalışır.
+    {
+        echo "Yaşlı";
+    }
+
+
+
+
+
